371851fc184b7916b7f8ca0d9157c79f63b78d2b
Welder Ressutti <welderessutti@gmail.com>
2025-02-21 11:20:53 -0300

    switching server port

--- src/main/java/br/com/fiap/api_diagnosis/framework/dto/event/consumer/GlycatedHemoglobinCreatedEventDto.java
+++ src/main/java/br/com/fiap/api_diagnosis/framework/dto/event/consumer/GlycatedHemoglobinCreatedEventDto.java
@@ -1,5 +1,6 @@
 package br.com.fiap.api_diagnosis.framework.dto.event.consumer;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class GlycatedHemoglobinCreatedEventDto {
 
+    @JsonAlias("idExame")
     private Long id;
+
+    @JsonAlias("idPaciente")
     private Long patientId;
+
+    @JsonAlias("resultadoExame")
     private Double result;
 }
